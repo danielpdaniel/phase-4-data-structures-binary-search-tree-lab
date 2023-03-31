3944340d075448437d85025b8ac70b61ab96d217
danielpdaniel <artofjquinn@gmail.com>
2023-03-31 14:16:15 -0400

    commit tree 8fe63f0702f1b0be46e7cfdbbd3622211d08b062

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -1,4 +1,5 @@
 require_relative './node'
+require 'pry'
 
 class BinarySearchTree
   attr_accessor :root
@@ -9,10 +10,59 @@ class BinarySearchTree
 
   def search(value)
     # your code here
+    nodes_to_search = @root ? [@root] : []
+  
+      while nodes_to_search.length > 0
+        current_node = nodes_to_search.shift
+
+        if current_node.nil?
+          return nil
+        elsif current_node.value == value
+          return current_node
+        else
+          if value < current_node.value
+            nodes_to_search.push(current_node.left)
+          elsif value > current_node.value
+            nodes_to_search.push(current_node.right)
+          end
+        end
+      end
   end
 
   def insert(value)
     # your code here
+    if @root.nil?
+      @root = Node.new(value)
+    end
+    nodes_to_search = [@root]
+
+    while nodes_to_search.length > 0
+      current_node = nodes_to_search.shift
+
+      if value == current_node.value
+        return current_node
+      else
+        if value > current_node.value
+          if current_node.right
+            nodes_to_search.push(current_node.right)       
+          else
+            new_node = Node.new(value)
+            current_node.right = new_node
+            return new_node
+          end
+        else
+          if value < current_node.value
+            if current_node.left
+              nodes_to_search.push(current_node.left)
+            else
+              new_node = Node.new(value)
+              current_node.left = new_node
+              return new_node
+            end
+          end
+        end
+      end
+    end 
   end
 
 end 
